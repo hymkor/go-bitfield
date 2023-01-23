dcbfb34098bdbf68c8e62fe33d85204f23b2ffff
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-01-23 22:01:25 +0900

    Add test for NG-Case

---
+++ main2_test.go
@@ -0,0 +1,37 @@
+package bitfield_test
+
+import (
+	"testing"
+
+	"github.com/hymkor/go-bitfield"
+)
+
+type DosDateNG1 struct {
+	Second int  `bit:"5"`
+	Min    uint `bit:"X"`
+	Hour   int  `bit:"5"`
+}
+
+func TestTestNg1(t *testing.T) {
+	var dt DosDateNG1
+	if err := bitfield.Read(0x7423, &dt); err == nil {
+		t.Fatal("Any error is not found in invalid structure")
+	} else {
+		println("OK: expect error:", err.Error())
+	}
+}
+
+type DosDateNG2 struct {
+	Second int  `bit:"5"`
+	Min    uint `bit:"6"`
+	Hour   *int `bit:"5"`
+}
+
+func TestTestNg2(t *testing.T) {
+	var dt DosDateNG2
+	if err := bitfield.Read(0x7423, &dt); err == nil {
+		t.Fatal("Any error is not found in invalid structure")
+	} else {
+		println("OK: expect error:", err.Error())
+	}
+}
